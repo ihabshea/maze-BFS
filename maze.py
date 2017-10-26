fcee88e08c94c8b1d87711c2d088a28c2010ede8
Ihab E. McShea <ihabsoft@gmail.com>
2017-10-26 16:05:29 -0600

    Add files via upload

---
+++ maze.py
@@ -0,0 +1,43 @@
+from collections import deque
+def mazeg(maze):
+    height = len(maze)
+    width = len(maze[0]) if height else 0
+    graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]} #adding all the possible positions
+    for row, col in graph.keys(): #adding the possible actions in a given position
+        if row < height - 1 and not maze[row + 1][col]:
+            graph[(row, col)].append(("D", (row + 1, col)))
+            graph[(row + 1, col)].append(("U", (row, col)))
+        if col < width - 1 and not maze[row][col + 1]:
+            graph[(row, col)].append(("R", (row, col + 1)))
+            graph[(row, col + 1)].append(("L", (row, col)))
+    return graph
+
+def find_path_bfs(maze,start,goal):
+    queue = deque([("", start)])
+    visited = set()
+    graph = mazeg(maze)
+    while queue:
+        path, current = queue.popleft()
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            queue.append((path + direction, neighbour))
+    return "Can't find a route."
+maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]] #0 : way #1: no way.
+
+print find_path_bfs(maze,(1,1),(8,10))
